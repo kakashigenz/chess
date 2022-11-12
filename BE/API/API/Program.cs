7d37e4cd0cef8a5345fdb9014202df47028241f3
giangle2809 <67696741+giangle2809@users.noreply.github.com>
2022-11-12 19:51:51 +0700

    chỉnh sửa lại code

---
+++ BE/API/API/Program.cs
@@ -0,0 +1,42 @@
+using API.Interface;
+using API.Model;
+using API.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+builder.Services.Configure<ChessOption>(builder.Configuration.GetSection("Chess"));
+
+// DI
+builder.Services.AddTransient<IChessService, ChessService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("MyPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
